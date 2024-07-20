9da09266b045df90bd390b5789699e8a441788df
abyssooo <168204989+abyssooo@users.noreply.github.com>
2024-07-20 17:20:59 +0900

    수정

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,69 +9,87 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     Animator anim;
+    private bool isGrounded;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-
+        isGrounded = false;
     }
 
     void Update()
     {
-        //Jump
-        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping") ) { 
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             anim.SetBool("isJumping", true);
-
+            isGrounded = false;
         }
 
-
-        //Stop Speed
+        // Stop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
 
-        //Direction Sprite
+        // Direction Sprite
         if (Input.GetButton("Horizontal"))
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
 
-        //Animation
-        if (Mathf.Abs(rigid.velocity.normalized.x) < 0.3)
+        // Animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
             anim.SetBool("isRunning", false);
         else
             anim.SetBool("isRunning", true);
-
     }
-    // Update is called once per frame
+
     void FixedUpdate()
     {
-        //Move
+        // Move
         float h = Input.GetAxisRaw("Horizontal");
+        rigid.velocity = new Vector2(h * maxSpeed, rigid.velocity.y);
 
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        // Landing Platform
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector2.down * 1.0f, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1.0f, LayerMask.GetMask("Platform"));
 
-        //Right Max Speed
-        if(rigid.velocity.x > maxSpeed)
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < maxSpeed*(-1)) //Left
-            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-
-        //Landing Platform
-        if(rigid.velocity.y < 0) {
-            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
-
-            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
-
-            if (rayHit.collider != null)
+            if (rayHit.collider != null && rayHit.distance < 0.5f)
             {
-                if (rayHit.distance < 0.5f)
-                    Debug.Log(rayHit.collider.name);
                 anim.SetBool("isJumping", false);
+                isGrounded = true;
             }
-        } 
-        
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            anim.SetBool("isJumping", false);
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Platform"))
+        {
+            isGrounded = false;
+        }
     }
 }
+
+
